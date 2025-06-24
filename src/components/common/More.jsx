8cde04b2088e43bafb7c736bda7bd96c639bf55f
Vaibhav Chopde <vchopde77@gmail.com>
2025-06-24 10:49:12 +0530

    minor bug fix

--- src/components/common/More.jsx
+++ src/components/common/More.jsx
@@ -9,7 +9,7 @@ const More = ({ album }) => {
   const { user } = useAuth();
   const [showMore, setShowMore] = useState(false);
   const [showDelModal, setShowDelModal] = useState(false);
-  const [showShareAlbum, setShowShareAlbum] = useState(false);
+  // const [showShareAlbum, setShowShareAlbum] = useState(false);
   const [showRenameModal, setShowRenameModal] = useState(false);
 
   const handleMoreDiv = (e) => {
@@ -33,12 +33,12 @@ const More = ({ album }) => {
               >
                 Rename Album
               </p>
-              <p
+              {/* <p
                 className="px-3 py-2 hover:bg-gray-300 cursor-pointer"
                 onClick={() => setShowShareAlbum(true)}
               >
                 Share Album
-              </p>
+              </p> */}
               <p
                 onClick={() => setShowDelModal(true)}
                 className="px-3 py-2 hover:bg-gray-300 cursor-pointer"
@@ -57,13 +57,13 @@ const More = ({ album }) => {
             />
           )}
 
-          {showShareAlbum && (
+          {/* {showShareAlbum && (
             <ShareAlbum
               isOpen={showShareAlbum}
               setIsOpen={setShowShareAlbum}
               albumId={album._id}
             />
-          )}
+          )} */}
 
           {showRenameModal && (
             <RenameModal
